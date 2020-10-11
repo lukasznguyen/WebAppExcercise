5bd6c1d0c11cfb46a27a7d3b9bb52a4c0f04b108
Tuan Viet Nguyen <lukasznguyen1999@gmail.com>
2020-10-11 15:12:42 +0200

    Added Lang_PARAM and modified prepareGreeting method

--- src/main/java/io/github/mat3e/HelloServlet.java
+++ src/main/java/io/github/mat3e/HelloServlet.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     private static final String NAME_PARAM = "name";
+    private static final String LANG_PARAM = "lang";
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 
     private HelloService service;
@@ -33,6 +34,8 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.info("Got request with parameters: " + req.getParameterMap());
-        resp.getWriter().write(service.prepareGreeting(req.getParameter(NAME_PARAM)));
+        var name = req.getParameter(NAME_PARAM);
+        var lang = req.getParameter(LANG_PARAM);
+        resp.getWriter().write(service.prepareGreeting(name, lang));
     }
 }
